e787fb71e01833626988899d31cbc6fc5ecd8982
Irina Sass <sass_is@sgmk-group.ru>
2023-04-16 22:12:09 +0700

    task-49.1 добавить хлебные крошки

--- front/src/pages/photo-gallery/PhotoGallery.jsx
+++ front/src/pages/photo-gallery/PhotoGallery.jsx
@@ -2,33 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { GalleryService } from '../../services/gallery.service';
 import styles from './PhotoGallery.module.scss';
-
-const events = [
-	{
-		theme_id: 1,
-		title: 'Event 1',
-		description: 'Description for event 1',
-		url_preview: 'https://via.placeholder.com/500x300?text=Photo+1',
-	},
-	{
-		theme_id: 2,
-		title: 'Event 2',
-		description: 'Description for event 2',
-		url_preview: 'https://via.placeholder.com/500x300?text=Photo+4',
-	},
-	{
-		theme_id: 3,
-		title: 'Event 1',
-		description: 'Description for event 3',
-		url_preview: 'https://via.placeholder.com/500x300?text=Photo+1',
-	},
-	{
-		theme_id: 4,
-		title: 'Event 2',
-		description: 'Description for event 4',
-		url_preview: 'https://via.placeholder.com/500x300?text=Photo+4',
-	},
-];
+import Breadcrumbs from '../../components/Breadcrumbs/Breadcrumbs';
 
 const PhotoGallery = () => {
 	const [gallery, setGallery] = useState(null);
@@ -44,7 +18,8 @@ const PhotoGallery = () => {
 	return !gallery ? (
 		<h1>{error}</h1>
 	) : (
-		<div className="container">
+			<div className="container">
+			<Breadcrumbs activePage="Фотогалерея" />
 			<h1 className="page-title">Фотогалерея</h1>
 			<div className={styles.gallery}>
 				{gallery.map((event) => (
